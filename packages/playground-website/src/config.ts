7c42f1de19ab76adc9a3133df24e6cde75bed6c1
jennypng <63012604+JennyPng@users.noreply.github.com>
2026-03-24 14:56:10 -0700

    add python's peerdeps to playground bundle

--- packages/playground-website/src/config.ts
+++ packages/playground-website/src/config.ts
@@ -15,6 +15,12 @@ export const TypeSpecPlaygroundConfig = {
     "@typespec/events",
     "@typespec/sse",
     "@typespec/xml",
+    "@azure-tools/typespec-client-generator-core",
+    "@azure-tools/typespec-autorest",
+    "@azure-tools/typespec-azure-core",
+    "@azure-tools/typespec-azure-resource-manager",
+    "@azure-tools/typespec-azure-rulesets",
+    "@typespec/http-client-python",
   ],
   samples,
 } as const;
