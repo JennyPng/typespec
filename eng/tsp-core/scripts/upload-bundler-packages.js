2e762688a6e457006bf07cb8801ab8c3f7cc1574
jennypng <63012604+JennyPng@users.noreply.github.com>
2026-03-26 15:16:19 -0700

    jk need all peerdeps

--- eng/tsp-core/scripts/upload-bundler-packages.js
+++ eng/tsp-core/scripts/upload-bundler-packages.js
@@ -1,9 +1,12 @@
 // @ts-check
+import { resolve } from "path";
 import {
   bundleAndUploadPackages,
   getPackageVersion,
 } from "../../../packages/bundle-uploader/dist/src/index.js";
 import { repoRoot } from "../../common/scripts/helpers.js";
+
+const pythonEmitterRoot = resolve(repoRoot, "packages/http-client-python");
 
 await bundleAndUploadPackages({
   repoRoot: repoRoot,
@@ -22,5 +25,37 @@ await bundleAndUploadPackages({
     "@typespec/events",
     "@typespec/sse",
     "@typespec/xml",
+  ],
+  extraPackages: [
+    {
+      name: "@azure-tools/typespec-client-generator-core",
+      rootDir: resolve(
+        pythonEmitterRoot,
+        "node_modules/@azure-tools/typespec-client-generator-core",
+      ),
+    },
+    {
+      name: "@azure-tools/typespec-azure-core",
+      rootDir: resolve(pythonEmitterRoot, "node_modules/@azure-tools/typespec-azure-core"),
+    },
+    {
+      name: "@azure-tools/typespec-autorest",
+      rootDir: resolve(pythonEmitterRoot, "node_modules/@azure-tools/typespec-autorest"),
+    },
+    {
+      name: "@azure-tools/typespec-azure-resource-manager",
+      rootDir: resolve(
+        pythonEmitterRoot,
+        "node_modules/@azure-tools/typespec-azure-resource-manager",
+      ),
+    },
+    {
+      name: "@azure-tools/typespec-azure-rulesets",
+      rootDir: resolve(pythonEmitterRoot, "node_modules/@azure-tools/typespec-azure-rulesets"),
+    },
+    {
+      name: "@typespec/http-client-python",
+      rootDir: pythonEmitterRoot,
+    },
   ],
 });
