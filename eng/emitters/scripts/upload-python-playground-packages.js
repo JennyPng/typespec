2e7394e88bacc677ec13967cd5d55e829056f214
jennypng <63012604+JennyPng@users.noreply.github.com>
2026-03-30 10:34:31 -0700

    minor

--- eng/emitters/scripts/upload-python-playground-packages.js
+++ eng/emitters/scripts/upload-python-playground-packages.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { uploadPythonPlaygroundPackages } from "../../../packages/bundle-uploader/dist/src/upload-python-packages.js";
+import { uploadPythonPlaygroundPackages } from "../../../packages/bundle-uploader/dist/src/upload-python-packages.ts";
 import { repoRoot } from "../../common/scripts/helpers.js";
 
 await uploadPythonPlaygroundPackages({ repoRoot });
